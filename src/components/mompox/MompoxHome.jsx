aebe56655cb90f2636db11c5e534cd4319cf9041
Kenneth3750 <110206946+Kenneth3750@users.noreply.github.com>
2026-03-09 14:42:19 -0500

    Swap Mompox SABIA image

--- src/components/mompox/MompoxHome.jsx
+++ src/components/mompox/MompoxHome.jsx
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { ArrowRight, Users, Video, MessageSquare } from "lucide-react";
 import { motion, AnimatePresence } from "framer-motion";
-import Mompox_AF from "../../assets/Mompox_AF.png";
+import MompoxImage from "../../assets/mompox.png";
 import MompoxHamburgerMenu from "./MompoxHamburgerMenu";
 
 const HeroCarousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const images = [
-    { src: Mompox_AF, alt: "Asistente de Atencion al Ciudadano", role: "Asistente Ciudadano" }
+    { src: MompoxImage, alt: "Asistente de Atencion al Ciudadano", role: "Asistente Ciudadano" }
   ];
 
   useEffect(() => {
